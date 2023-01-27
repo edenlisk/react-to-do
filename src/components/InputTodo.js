fa4bb7897e48406c66b4df33ae3b820054712b4e
edenlisk <nkumbuyenock@gmail.com>
2023-01-27 15:54:30 +0200

    Create basic structure of todo list

---
+++ src/components/InputTodo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaPlusCircle } from 'react-icons/fa';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const InputTodo = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default InputTodo;
